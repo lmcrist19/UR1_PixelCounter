7c1ed240ba4374cd1eba5a621f11385f8f348d1d
lmcrist19 <98054225+lmcrist19@users.noreply.github.com>
2022-02-14 18:11:26 -0500

    Add files via upload

---
+++ PixelCounter/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace PixelCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rawView = new System.Windows.Forms.PictureBox();
+            this.thresholdView = new System.Windows.Forms.PictureBox();
+            this.thresholdTrackBar = new System.Windows.Forms.TrackBar();
+            this.slice1Label = new System.Windows.Forms.Label();
+            this.slice2Label = new System.Windows.Forms.Label();
+            this.slice3Label = new System.Windows.Forms.Label();
+            this.slice4Label = new System.Windows.Forms.Label();
+            this.slice5Label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.rawView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rawView
+            // 
+            this.rawView.Location = new System.Drawing.Point(12, 12);
+            this.rawView.Name = "rawView";
+            this.rawView.Size = new System.Drawing.Size(351, 247);
+            this.rawView.TabIndex = 0;
+            this.rawView.TabStop = false;
+            // 
+            // thresholdView
+            // 
+            this.thresholdView.Location = new System.Drawing.Point(437, 12);
+            this.thresholdView.Name = "thresholdView";
+            this.thresholdView.Size = new System.Drawing.Size(351, 247);
+            this.thresholdView.TabIndex = 1;
+            this.thresholdView.TabStop = false;
+            // 
+            // thresholdTrackBar
+            // 
+            this.thresholdTrackBar.Location = new System.Drawing.Point(245, 341);
+            this.thresholdTrackBar.Maximum = 255;
+            this.thresholdTrackBar.Name = "thresholdTrackBar";
+            this.thresholdTrackBar.Size = new System.Drawing.Size(533, 56);
+            this.thresholdTrackBar.TabIndex = 2;
+            this.thresholdTrackBar.TickFrequency = 10;
+            this.thresholdTrackBar.Scroll += new System.EventHandler(this.thresholdTrackBar_Scroll_1);
+            // 
+            // slice1Label
+            // 
+            this.slice1Label.AutoSize = true;
+            this.slice1Label.Location = new System.Drawing.Point(30, 309);
+            this.slice1Label.Name = "slice1Label";
+            this.slice1Label.Size = new System.Drawing.Size(50, 17);
+            this.slice1Label.TabIndex = 3;
+            this.slice1Label.Text = "Slice 1";
+            // 
+            // slice2Label
+            // 
+            this.slice2Label.AutoSize = true;
+            this.slice2Label.Location = new System.Drawing.Point(30, 326);
+            this.slice2Label.Name = "slice2Label";
+            this.slice2Label.Size = new System.Drawing.Size(50, 17);
+            this.slice2Label.TabIndex = 4;
+            this.slice2Label.Text = "Slice 2";
+            // 
+            // slice3Label
+            // 
+            this.slice3Label.AutoSize = true;
+            this.slice3Label.Location = new System.Drawing.Point(30, 343);
+            this.slice3Label.Name = "slice3Label";
+            this.slice3Label.Size = new System.Drawing.Size(50, 17);
+            this.slice3Label.TabIndex = 5;
+            this.slice3Label.Text = "Slice 3";
+            // 
+            // slice4Label
+            // 
+            this.slice4Label.AutoSize = true;
+            this.slice4Label.Location = new System.Drawing.Point(30, 360);
+            this.slice4Label.Name = "slice4Label";
+            this.slice4Label.Size = new System.Drawing.Size(50, 17);
+            this.slice4Label.TabIndex = 6;
+            this.slice4Label.Text = "Slice 4";
+            // 
+            // slice5Label
+            // 
+            this.slice5Label.AutoSize = true;
+            this.slice5Label.Location = new System.Drawing.Point(30, 377);
+            this.slice5Label.Name = "slice5Label";
+            this.slice5Label.Size = new System.Drawing.Size(50, 17);
+            this.slice5Label.TabIndex = 7;
+            this.slice5Label.Text = "Slice 5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.slice5Label);
+            this.Controls.Add(this.slice4Label);
+            this.Controls.Add(this.slice3Label);
+            this.Controls.Add(this.slice2Label);
+            this.Controls.Add(this.slice1Label);
+            this.Controls.Add(this.thresholdTrackBar);
+            this.Controls.Add(this.thresholdView);
+            this.Controls.Add(this.rawView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rawView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox rawView;
+        private System.Windows.Forms.PictureBox thresholdView;
+        private System.Windows.Forms.TrackBar thresholdTrackBar;
+        private System.Windows.Forms.Label slice1Label;
+        private System.Windows.Forms.Label slice2Label;
+        private System.Windows.Forms.Label slice3Label;
+        private System.Windows.Forms.Label slice4Label;
+        private System.Windows.Forms.Label slice5Label;
+    }
+}
+
---
+++ PixelCounter/Form1.cs
@@ -0,0 +1,112 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelCounter
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        private int _threshold = 150;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture(0);
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+            thresholdTrackBar.Value = _threshold;
+        }
+        private void DisplayWebcam()
+        {
+            while(_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+                int newHeight = (frame.Size.Height * rawView.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(rawView.Size.Width, newHeight);
+                CvInvoke.Resize(frame, frame, newSize);
+                rawView.Image = frame.Bitmap;
+
+                Mat grayFrame = new Mat();
+                Mat binaryFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                CvInvoke.Threshold(grayFrame, binaryFrame, _threshold, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+
+                thresholdView.Image = binaryFrame.Bitmap;
+
+                int slice1WhitePixels = 0;
+                int slice2WhitePixels = 0;
+                int slice3WhitePixels = 0;
+                int slice4WhitePixels = 0;
+                int slice5WhitePixels = 0;
+
+                Image<Gray, byte> img = binaryFrame.ToImage<Gray, byte>();
+                
+                for (int slice1x = 0; slice1x< binaryFrame.Width / 5; slice1x++)
+                {
+                    for(int slice1y = 0; slice1y < binaryFrame.Height; slice1y++)
+                    {
+                        if (img.Data[slice1y, slice1x, 0] == 255) slice1WhitePixels++; //slice 1 white pixel counter
+                    }
+                }
+                for (int slice2x = binaryFrame.Width / 5; slice2x < (2 * binaryFrame.Width) / 5; slice2x++)
+                {
+                    for (int slice2y = 0; slice2y < binaryFrame.Height; slice2y++)
+                    {
+                        if (img.Data[slice2y, slice2x, 0] == 255) slice2WhitePixels++; //slice 2 white pixel counter
+                    }
+                }
+                for (int slice3x = (2 * binaryFrame.Width) / 5; slice3x < (3 * binaryFrame.Width) / 5; slice3x++)
+                {
+                    for (int slice3y = 0; slice3y < binaryFrame.Height; slice3y++)
+                    {
+                        if (img.Data[slice3y, slice3x, 0] == 255) slice3WhitePixels++; //slice 3 white pixel counter
+                    }
+                }
+                for (int slice4x = (3 * binaryFrame.Width) / 5; slice4x < (4 * binaryFrame.Width) / 5; slice4x++)
+                {
+                    for (int slice4y = 0; slice4y < binaryFrame.Height; slice4y++)
+                    {
+                        if (img.Data[slice4y, slice4x, 0] == 255) slice4WhitePixels++; //slice 4 white pixel counter
+                    }
+                }
+                for (int slice5x = (4 * binaryFrame.Width) / 5; slice5x <  binaryFrame.Width; slice5x++)
+                {
+                    for (int slice5y = 0; slice5y < binaryFrame.Height; slice5y++)
+                    {
+                        if (img.Data[slice5y, slice5x, 0] == 255) slice5WhitePixels++; //slice 5 white pixel counter
+                    }
+                }
+                Invoke(new Action(() =>
+                {
+                    slice1Label.Text = $"Slice 1: {slice1WhitePixels} white pixels";
+                    slice2Label.Text = $"Slice 2: {slice2WhitePixels} white pixels";
+                    slice3Label.Text = $"Slice 3: {slice3WhitePixels} white pixels";
+                    slice4Label.Text = $"Slice 4: {slice4WhitePixels} white pixels";
+                    slice5Label.Text = $"Slice 5: {slice5WhitePixels} white pixels";
+                }));
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _captureThread.Abort();
+        }
+        private void thresholdTrackBar_Scroll_1(object sender, EventArgs e)
+        {
+           _threshold = thresholdTrackBar.Value;
+        }
+    }
+}
